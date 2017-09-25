fd236e6498592daf809c68f2c6e9ef2753986ff2
Rohit Parab <parab.rohit@gmail.com>
2017-09-25 15:53:58 +0530

    Update sumOfMultiples.go

--- sumOfMultiples.go
+++ sumOfMultiples.go
@@ -1,18 +1,21 @@
 package main
-import ( "fmt"; "time")
-func main() 
-{ start := time.Now() 
-sum := 0 for i := 0; i < 1000; i++ {  
-if (i%3 == 0) && (i%5 == 0) {   
-continue  
-}   
-if (i%3 == 0) {   
-sum += i  
-}  
-if (i%5 == 0) {
-sum += i  }
+import ( "fmt" "time")
+func main() {
+ start := time.Now() 
+ sum := 0 
+ for i := 0; i < 1000; i++ {
+  if (i%3 == 0) && (i%5 == 0) {
+   sum += i   
+   continue  
+  }  
+  if i%3 == 0 {
+   sum += i  
+  }
+  if i%5 == 0 {
+   sum += i  
+  }
  } 
  end := time.Now() 
  tot_time := end.Sub(start) 
- fmt.Println(sum , tot_time)
- }
+ fmt.Println(sum, tot_time)
+}
